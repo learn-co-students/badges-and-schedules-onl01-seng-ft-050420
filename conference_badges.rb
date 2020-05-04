6950e6e0a26c9b89e75201f128a28869bddec237
Luke Chen <luke.chen.2010@gmail.com>
2020-05-04 20:31:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = Array.new
+  
+  names.each do |name|
+    badges.push(badge_maker(name))
+  end
+  
+  return badges
+end
+
+def assign_rooms(speakers)
+  assignments = Array.new
+  
+  speakers.each do |speaker|
+    room_num = speakers.index(speaker) + 1
+    assignments.push("Hello, #{speaker}! You'll be assigned to room #{room_num}!")
+  end
+  
+  return assignments
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
+  
+  badges.each do |badge|
+    puts badge
+  end
+  
+  rooms.each do |room|
+    puts room
+  end
+end
