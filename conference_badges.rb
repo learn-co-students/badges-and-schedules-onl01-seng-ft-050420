b95b6ea082f836c6b81b7294002a86dc347a9a93
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-04 18:32:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  ary = []
+  attendees.each { |name| ary.push("Hello, my name is #{name}.") }
+  return ary
+end
+
+def assign_rooms(attendees)
+  ary = []
+  attendees.each_with_index {|name, i| ary.push("Hello, #{name}! You'll be assigned to room #{i+1}!")}
+  return ary
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|badge| puts "#{badge}"}
+  assign_rooms(attendees).each {|assign| puts "#{assign}"}
+end
